c6e20df44acdf9b5215583b54c21a2b7af0ea9dd
Maximilian Kapra <maximilian@kapra.de>
2021-04-23 10:42:05 +0200

    Final touch

--- buildpdf.sh
+++ buildpdf.sh
@@ -1,1 +1,2 @@
-pandoc -o document.pdf --highlight-style breezeDark document.md
+pandoc -o output/document.pdf --highlight-style breezeDark document.md
+pandoc -o output/document.html --highlight-style breezeDark document.md
